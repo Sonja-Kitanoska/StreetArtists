14e51eb4e258eabb3d1516bc718be4aa812da974
Sonja Kitanoska <avramoskamkd@gmail.com>
2024-12-25 17:15:32 +0100

    Add new item div added to the artistItemsPage

--- src/pages/artistItemsPage/artistItemsPage.js
+++ src/pages/artistItemsPage/artistItemsPage.js
@@ -3,6 +3,8 @@ import { updateHeader } from "../../utils/globals.js";
 import { items } from "../../../data/db.js";
 import { getArtist } from "../../utils/globals.js";
 
+const addNewItemDiv = document.querySelector(".add-new-item-div");
+
 export function initArtistItemsPage() {
 	const currentArtist = getArtist();
 	updateHeader("artist");
@@ -10,4 +12,8 @@ export function initArtistItemsPage() {
 	const artistItems = items.filter((item) => item.artist === currentArtist);
 
 	renderCards(artistItems, "artist");
+
+	addNewItemDiv.addEventListener("click", () => {
+		location.hash = "#artistAddNewItemPage";
+	});
 }
