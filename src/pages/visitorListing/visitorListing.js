b0652914983f6f9b00d5cd15f948bb11dc1646a7
Sonja Kitanoska <avramoskamkd@gmail.com>
2024-12-28 17:54:26 +0100

    same renderCards function for artists and visitors

--- src/pages/visitorListing/visitorListing.js
+++ src/pages/visitorListing/visitorListing.js
@@ -1,12 +1,10 @@
-import { getItems, setItems, updateHeader } from "../../utils/globals.js";
-// import { itemTypes, items } from "../../../data/db.js";
+import { getItems, updateHeader } from "../../utils/globals.js";
 import { itemTypes } from "../../../data/db.js";
+import { renderCards } from "../../utils/cards.js";
 
 const checkContainer = document.querySelector(".check-container");
 const artistSelect = document.querySelector("#artist");
 const typeSelect = document.querySelector("#type");
-const visitorListingSection = document.querySelector("#visitorListing");
-const cardsContainer = visitorListingSection.querySelector(".cards-container");
 
 export function resetFilters() {
 	artistSelect.innerHTML = '<option value="">Choose</option>';
@@ -32,62 +30,11 @@ export function initVisitorListing() {
 
 	document.body.style.overflow = "auto";
 
-	function renderCards(items) {
-		cardsContainer.innerHTML = "";
-
-		const publishedItems = items.filter((item) => item.isPublished);
-		if (publishedItems.length === 0) {
-			cardsContainer.innerHTML = `
-				<div class="no-results">
-					<p>No items matched your filters.</p>
-					<p>Please adjust the filters and try again.</p>
-				</div>`;
-			cardsContainer.classList.add("h-100");
-			return;
-		}
-
-		publishedItems.forEach((item, index) => {
-			const artistCard = document.createElement("div");
-			artistCard.classList.add("artist-card", "mt-4");
-			artistCard.setAttribute("id", `${item.id}`);
-
-			const cardBodyClass =
-				index % 2 === 0 ? "odd-index-colors" : "even-index-colors";
-
-			const priceSpanClass =
-				index % 2 === 0 ? "even-index-colors" : "odd-index-colors";
-
-			artistCard.innerHTML = `
-				<div class="img-container">
-					<img
-						src="${item.image}"
-						alt="${item.artist}'s image"
-						class="w-100 h-100"
-					/>
-				</div>
-				<div class="py-2 px-3 ${cardBodyClass}">
-					<div class="d-flex justify-content-between align-items-center">
-						<h3 class="reenie-beanie">${item.artist}</h3>
-						<span class="price-span ${priceSpanClass} py-1 px-2 font-size-12">$${item.price}</span>
-					</div>
-
-					<h6 class="font-size-14">${item.title}</h6>
-					<p class="mb-0 font-size-11">
-						${item.description}
-					</p>
-				</div>`;
-			cardsContainer.appendChild(artistCard);
-		});
-	}
-
-	// renderCards(itemsList);
-
 	const filtersImage = document.querySelector(".filters-image");
 	filtersImage.addEventListener("click", () => {
 		location.hash = "#visitorFilters";
 	});
 
-	// artistSelect.innerHTML = '<option value="">Choose</option>';
 	fetch("https://jsonplaceholder.typicode.com/users")
 		.then((response) => response.json())
 		.then((users) => {
@@ -100,7 +47,6 @@ export function initVisitorListing() {
 			});
 		});
 
-	// typeSelect.innerHTML = '<option value="">Choose</option>';
 	itemTypes.forEach((type) => {
 		const option = document.createElement("option");
 		option.value = type.toLowerCase();
@@ -125,13 +71,6 @@ export function initVisitorListing() {
 			.value.trim()
 			.toLowerCase();
 
-		console.log("Filters Applied:");
-		console.log("Title:", titleFilter || "Any");
-		console.log("Artist:", artistFilter || "Any");
-		console.log("Min Price:", minPrice);
-		console.log("Max Price:", maxPrice);
-		console.log("Type:", typeFilter || "Any");
-
 		// Filter logic
 		const filteredItems = itemsList.filter((item) => {
 			const matchedTitle = item.title.toLowerCase().includes(titleFilter);
@@ -142,12 +81,6 @@ export function initVisitorListing() {
 			const matchedType = typeFilter.toLowerCase()
 				? item.type.toLowerCase() === typeFilter
 				: true;
-
-			console.log(`Checking item: ${item.title}`);
-			console.log(`Title Matched: ${matchedTitle}`);
-			console.log(`Artist Matched: ${matchedArtist}`);
-			console.log(`Price Matched: ${matchedPrice}`);
-			console.log(`Type Matched: ${matchedType}`);
 			return matchedTitle && matchedArtist && matchedPrice && matchedType;
 		});
 		console.log("Filtered Items:", filteredItems);
