e00e680cc77009d5bab8b9abfff502749c5057d5
Sonja Kitanoska <avramoskamkd@gmail.com>
2025-01-08 10:42:05 +0100

    Visitor homepage styling corrections

--- src/pages/visitorHomePage/visitorHomePage.js
+++ src/pages/visitorHomePage/visitorHomePage.js
@@ -1,7 +1,6 @@
 import { updateHeader } from "../../utils/header.js";
 
 export function initVisitorHomePage() {
-	console.log("visitorHomePage");
 	updateHeader("visitor");
 
 	const infiniteSlider = document.querySelector(".infinite-slider");
